49667fef04a6f14c0a1224c5771b2e58528a196e
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-06-01 18:47:17 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -109,7 +109,7 @@ server <- function(input, output,session) {
    # session$reload()
     #return()
   #})
-  output$table<-renderDT(tot_tab, filter = 'top',rownames=F,
+  output$table<-renderDT(tot_tab, filter = 'top',rownames=F,selection="none",
                          #,server = T,
                          options = list(autoWidth = F,scrollX=T)
                          )#,server = TRUE,options=list(pageLength = 100,scrollX = T),  escape = FALSE
