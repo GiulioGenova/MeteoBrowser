f2f2507ac95f3f08a52e40591928ba4967a6164d
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-06-19 12:51:34 +0200

    Update about.R

--- app/about.R
+++ app/about.R
@@ -4,10 +4,10 @@ tags$html(
   ),
   tags$body(
     h3("About the data"),
-    p("This app provides a user-friendly interface to download data from the Open Data S&#369;dtirol."),
-    p("The Open Data S&#369;dtirol API and its documentation can be found at the Province of Bozen web site."),
+    p("This app provides a user-friendly interface to download data from the Open Data S&#252;dtirol."),
+    p("The Open Data S&#252;dtirol API and its documentation can be found at the Province of Bozen web site."),
     p(a( href='http://daten.buergernetz.bz.it/it/dataset/misure-meteo-e-idrografiche',
-         target='_blank','Open Data S&#369;dtirol - Meteo data web service' )),
+         target='_blank','Open Data S&#252;dtirol - Meteo data web service' )),
     p("Once selected the stations and sensors by filtering the table and chosen a date range you are ready to download (the dataset starts in the beginning of 2016). When download is complete, a save csv icon pops up. You can choose between different time aggregations. raw preserves the timestamp of the sensor you are downloading (usually 5 or 10 minutes). For the other time aggregation options you will get the mean, maximum, minimum and sum values for each sensor in the selected timespan. You can also choose between wide or long data. In wide data you get a column for every sensor, in long data you get a factor column called Sensor and a column Value which stores the measurements."),
     br(),
     h3("Acknowledgements"),
