6e0f954ff296599bc6ed9589b66247cb44258345
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-07-18 14:37:18 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -248,32 +248,35 @@ server <- function(input, output,session) {
     ids<-input$table_rows_all
     
     station<-unique(tot_tab$NAME_D[ids])%>%as.character
+    scode<-unique(tot_tab$SCODE[ids])%>%as.character
+    
     #########################################################
     if(input$spatialSelection){#FALSE
      
-     stations_sp <- getMeteoStat(format = "spatial")%>%filter(NAME_D%in%station)
+     stations_sp <- getMeteoStat(format = "spatial")%>%filter(SCODE%in%scode)
      req(input$map_draw_stop)
-
+     
+     
      #get the coordinates of the polygon
      polygon_coordinates <- input$map_draw_new_feature$geometry$coordinates[[1]]
       
-     drawn_polygon <- Polygon(do.call(rbind,lapply(polygon_coordinates,function(x){c(x[[1]][1],x[[2]][1])})))
-     sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
+    drawn_polygon <- Polygon(do.call(rbind,lapply(polygon_coordinates,function(x){c(x[[1]][1],x[[2]][1])})))
+    sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
 
-     # set coords as latlong then transform to leaflet projection
-     proj4string(sp) <- LL
-     polyre <- spTransform(sp, leaf.proj)
+    # set coords as latlong then transform to leaflet projection
+    proj4string(sp) <- LL
+    polyre <- spTransform(sp, leaf.proj)
     
-     stations_sp<-spTransform(stations_sp,leaf.proj)
+    stations_sp<-spTransform(stations_sp,leaf.proj)
        
-     selected_stats <- stations_sp %over% polyre
+    selected_stats <- stations_sp %over% polyre
 
-     sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
+    sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
 
-     station<-unique(sp_sel$NAME_D) %>% as.character
+    station<-unique(sp_sel$NAME_D) %>% as.character
        #}
     
-     }
+      }
     
     #mssg<- paste("You have selected the following stations", station)
     mssg<- paste(station,collapse="; ")
