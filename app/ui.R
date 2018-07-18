25f3a469d5cd36b135690ba6dcf779784e769f67
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-07-18 16:44:15 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -99,8 +99,8 @@ ui <- dashboardPage(#useShinyjs(),
                   # choices = list("NO","YES"))
               ),
                    
-                box(width = 8,leafletOutput(actionButton("deletebtn", "remove drawn"),"map"),
-                    
+                box(width = 8,leafletOutput("map"),
+                    conditionalPanel(condition ="spatialSelection",actionButton("deletebtn", "remove drawn")),
                     helpText("You have selected the following stations:"),
                     verbatimTextOutput("selected_list"),
                     helpText("And the following parameters (TYPE):"),
