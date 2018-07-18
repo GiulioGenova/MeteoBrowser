bf0b2bd602df07f54a9e0484f5e63fdaabfe4f0a
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-07-18 15:21:51 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -1,4 +1,3 @@
-
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("readr")) install.packages("readr")
 if (!require("tibble")) install.packages("tibble")
@@ -76,100 +75,93 @@ se_spread<-se %>% dplyr::select(SCODE,TYPE,UNIT,VALUE) %>%
   unite(VALUE,VALUE,UNIT,sep=" ") %>% 
   spread(TYPE,VALUE)
 
-#stations_sp <- getMeteoStat(format = "spatial")
+
 
 server <- function(input, output,session) {
-  #observeEvent(input$stop,{
-   # session$reload()
-    #return()
-  #})
   
   output$table<-DT::renderDT({
-  datatable(tot_tab, filter = 'top',rownames=F,selection="none",
-                         options = list(autoWidth = F,scrollX=T)
-                         ) %>% 
-    formatStyle(c("TYPE", "DESC_D", "DESC_I","UNIT"),
-                                     backgroundColor = "#edf5e1")
-})
+    datatable(tot_tab, filter = 'top',rownames=F,selection="none",
+              options = list(autoWidth = F,scrollX=T)
+    ) %>% 
+      formatStyle(c("TYPE", "DESC_D", "DESC_I","UNIT"),
+                  backgroundColor = "#edf5e1")
+  })
   
-  #"#c3cfe6"
-  #output$table<-renderDT(tot_tab, filter = 'top',rownames=F,selection="none",
-   #                      options = list(autoWidth = F,scrollX=T)
-    #                     )%>%formatStyle(c("TYPE", "DESC_D", "DESC_I","UNIT"),
-     #                                 backgroundColor = "grey")
-  #,server = TRUE,options=list(pageLength = 100,scrollX = T),  escape = FALSE
   
-  ## connenction and query on province database
   D <- reactiveValues(documents = NULL)
-  #inputdb=reactive({
-    observeEvent(input$refresh,{
-    ids<-input$table_rows_all
-    #nstations<-length(ids)%>%as.numeric
-    #nstations<-10
-    #ids<-c(1,3,7,24)
-    #station<-tot_tab[ids,'SCODE']%>%unique#%>%as.vector
-    #sensors<-tot_tab[ids,'TYPE']%>%unique
-    station<-unique(tot_tab$SCODE[ids])%>%as.character
-    sensors<-unique(tot_tab$TYPE[ids])%>%as.character
+  
+  StatSens<-reactive({
+  
+  ids<-input$table_rows_all
+  
+  station<-unique(tot_tab$SCODE[ids])%>%as.character
+  sensors<-unique(tot_tab$TYPE[ids])%>%as.character
+  #########################################################
+  if(input$spatialSelection){#FALSE
     
-    datestart<-as.character(input$daterange[1])
-    dateend<-as.character(input$daterange[2])
-    #out_dir="H:/Projekte/SBR/04_Data/06_daily_resample"
-    round=input$round
-      
-    #################################################################################
-     #Spatial selection
-     if(input$spatialSelection){#FALSE
-     
-     stations_sp <- getMeteoStat(format = "spatial")%>%filter(SCODE%in%station)
-     req(input$map_draw_stop)
-     
-     
-     #get the coordinates of the polygon
-     polygon_coordinates <- input$map_draw_new_feature$geometry$coordinates[[1]]
-      
+    stations_sp <- getMeteoStat(format = "spatial")%>%filter(SCODE%in%station)
+    req(input$map_draw_stop)
+    
+    
+    #get the coordinates of the polygon
+    polygon_coordinates <- input$map_draw_new_feature$geometry$coordinates[[1]]
+    
     drawn_polygon <- Polygon(do.call(rbind,lapply(polygon_coordinates,function(x){c(x[[1]][1],x[[2]][1])})))
     sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
-
+    
     # set coords as latlong then transform to leaflet projection
     proj4string(sp) <- LL
     polyre <- spTransform(sp, leaf.proj)
     
     stations_sp<-spTransform(stations_sp,leaf.proj)
-       
+    
     selected_stats <- stations_sp %over% polyre
-
+    
     sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
-
+    
     station<-unique(sp_sel$SCODE) %>% as.character
-       #}
+    #}
+    
     filterForSensor<-tot_tab[ids,]%>% dplyr::filter(SCODE%in%station)
     sensors<-unique(filterForSensor$TYPE)%>%as.character
-      }
-    #################################################################################  
+  }
+  return(list(station,sensors))
+  })
+  
+  
+  
+  
+  
+  observeEvent(input$refresh,{
+    ids<-input$table_rows_all
+
+    station<-StatSens()[[1]]
+    sensors<-StatSens()[[2]]
+    
+    datestart<-as.character(input$daterange[1])
+    dateend<-as.character(input$daterange[2])
+
+    round=input$round
+ 
     nstations<-length(station)%>%as.numeric*length(sensors)%>%as.numeric
-      
-      
-      
+    
+    
     if(as_date(datestart)<=dateend){
-    withProgress(message = 'Getting data', value = 0, {
-    db<-dwnld(station=station,datestart=datestart,dateend=dateend,sensors=sensors,nstations=nstations)#round=round,
-    })#
+      withProgress(message = 'Getting data', value = 0, {
+        db<-dwnld(station=station,datestart=datestart,dateend=dateend,sensors=sensors,nstations=nstations)#round=round,
+      })#
     }else{db<-"Error in selecting the date range. First date must be earlier than last date"}
     D$documents <- list(db)
     
-    })
+  })
   
-  #downloaded <- eventReactive(input$refresh,{#,input$daterange,input$round,input$Station
-   # as.data.frame(inputdb())
-    #})
   
-    observeEvent(input$stop,{
-      session$reload()
-      return()
-    })
-    
-    
+  observeEvent(input$stop,{
+    session$reload()
+    return()
+  })
+  
+  
   output$map<-renderLeaflet({
     ids<-input$table_rows_all
     station<-unique(tot_tab$SCODE[ids])%>%as.character
@@ -178,17 +170,17 @@ server <- function(input, output,session) {
     m<-plotMeteoLeaflet(stations_sel)
     
     if(input$spatialSelection){#FALSE
-    m <- m %>% addDrawToolbar(
-               targetGroup='draw',
-               polylineOptions=FALSE,
-               markerOptions = FALSE,
-               circleOptions = FALSE,
-               rectangleOptions =FALSE,
-               circleMarkerOptions =FALSE)%>%#
-               addMeasure(position = "topleft",primaryLengthUnit = "meters")%>%
-               addLayersControl(overlayGroups = c('draw'),
-               options = layersControlOptions(collapsed = FALSE),position = "topleft")
-      }
+      m <- m %>% addDrawToolbar(
+        targetGroup='draw',
+        polylineOptions=FALSE,
+        markerOptions = FALSE,
+        circleOptions = FALSE,
+        rectangleOptions =FALSE,
+        circleMarkerOptions =FALSE)%>%#
+        addMeasure(position = "topleft",primaryLengthUnit = "meters")%>%
+        addLayersControl(overlayGroups = c('draw'),
+                         options = layersControlOptions(collapsed = FALSE),position = "topleft")
+    }
     m
     
   })
@@ -197,140 +189,41 @@ server <- function(input, output,session) {
     datestart<-as.character(input$daterange[1])
     dateend<-as.character(input$daterange[2])
     if(as_date(datestart)>as_date(dateend)){
-    messagedate<-"Error in selecting the date range. First date must be earlier than last date"}else{NULL}
+      messagedate<-"Error in selecting the date range. First date must be earlier than last date"}else{NULL}
   })
   
   output$selected<-renderText({
-    ids<-input$table_rows_all
     
-    station<-unique(tot_tab$SCODE[ids])%>%as.character
-    nstation<-unique(tot_tab$SCODE[ids])%>%as.character %>% length
-    nsensors<-unique(tot_tab$TYPE[ids])%>%as.character %>% length
-    #########################################################
-    if(input$spatialSelection){#FALSE
-     
-     stations_sp <- getMeteoStat(format = "spatial")%>%filter(SCODE%in%station)
-     req(input$map_draw_stop)
-     
-     
-     #get the coordinates of the polygon
-     polygon_coordinates <- input$map_draw_new_feature$geometry$coordinates[[1]]
-      
-    drawn_polygon <- Polygon(do.call(rbind,lapply(polygon_coordinates,function(x){c(x[[1]][1],x[[2]][1])})))
-    sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
-
-    # set coords as latlong then transform to leaflet projection
-    proj4string(sp) <- LL
-    polyre <- spTransform(sp, leaf.proj)
-    
-    stations_sp<-spTransform(stations_sp,leaf.proj)
-       
-    selected_stats <- stations_sp %over% polyre
-
-    sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
-
-    station<-unique(sp_sel$SCODE) %>% as.character
-       #}
-    nstation<-length(station)%>%as.character
-    filterForSensor<-tot_tab[ids,]%>% dplyr::filter(SCODE%in%station)
-    sensors<-unique(filterForSensor$TYPE)%>%as.character
-    nsensors<-length(sensors)%>%as.character
-      }
-    
+    station<-StatSens()[[1]]
+    sensors<-StatSens()[[2]]
+    nstation<-length(station)%>%as.numeric
+    nsensors<-length(sensors)%>%as.numeric
     
     if(nstation==1) stat<-" station" else{stat<- " stations"}
     if(nsensors==1) param<-" parameter" else{param<- " parameters"}
+    
     mssg<- paste("You have selected n° ", nstation,stat," and ",nsensors,param)
     
   })
   
   output$selected_list<-renderText({
-    ids<-input$table_rows_all
     
-    station<-unique(tot_tab$NAME_D[ids])%>%as.character
-    scode<-unique(tot_tab$SCODE[ids])%>%as.character
+    station<-StatSens()[[1]]
     
-    #########################################################
-    if(input$spatialSelection){#FALSE
-     
-     stations_sp <- getMeteoStat(format = "spatial")%>%filter(SCODE%in%scode)
-     req(input$map_draw_stop)
-     
-     
-     #get the coordinates of the polygon
-     polygon_coordinates <- input$map_draw_new_feature$geometry$coordinates[[1]]
-      
-    drawn_polygon <- Polygon(do.call(rbind,lapply(polygon_coordinates,function(x){c(x[[1]][1],x[[2]][1])})))
-    sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
-
-    # set coords as latlong then transform to leaflet projection
-    proj4string(sp) <- LL
-    polyre <- spTransform(sp, leaf.proj)
-    
-    stations_sp<-spTransform(stations_sp,leaf.proj)
-       
-    selected_stats <- stations_sp %over% polyre
-
-    sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
-
-    station<-unique(sp_sel$NAME_D) %>% as.character
-       #}
-    
-      }
-    
-    #mssg<- paste("You have selected the following stations", station)
     mssg<- paste(station,collapse="; ")
-})
+    
+  })
   
   output$selected_listSensors<-renderText({
-    ids<-input$table_rows_all
     
-    station<-unique(tot_tab$SCODE[ids])%>%as.character
-    sensors<-unique(tot_tab$TYPE[ids])%>%as.character
-    #########################################################
-    if(input$spatialSelection){#FALSE
-     
-     stations_sp <- getMeteoStat(format = "spatial")%>%filter(SCODE%in%station)
-     req(input$map_draw_stop)
-     
-     
-     #get the coordinates of the polygon
-     polygon_coordinates <- input$map_draw_new_feature$geometry$coordinates[[1]]
-      
-    drawn_polygon <- Polygon(do.call(rbind,lapply(polygon_coordinates,function(x){c(x[[1]][1],x[[2]][1])})))
-    sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
-
-    # set coords as latlong then transform to leaflet projection
-    proj4string(sp) <- LL
-    polyre <- spTransform(sp, leaf.proj)
+    sensors<-StatSens()[[2]]
     
-    stations_sp<-spTransform(stations_sp,leaf.proj)
-       
-    selected_stats <- stations_sp %over% polyre
-
-    sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
-
-    station<-unique(sp_sel$SCODE) %>% as.character
-       #}
-    filterForSensor<-tot_tab[ids,]%>% dplyr::filter(SCODE%in%station)
-    sensors<-unique(filterForSensor$TYPE)%>%as.character
-     }
-    
-    #mssg<- paste("You have selected the following stations", station)
     mssg<- paste(sensors,collapse="; ")
-})
-  
-  #observe({
-  #  click<-input$map_marker_click
-   # if(is.null(click))
-   #   return()
     
-   # text2<-paste("You've selected point ", click$id,click$lat,click$lng)
-   # output$Click_text<-renderText({
-   # text2
-   # })
- # })
-   
+  })
+  
+ 
+  
   output$rightdate <-reactive({
     datestart<- input$daterange[1] %>% as.character %>% as_date
     dateend<- input$daterange[2] %>% as.character %>% as_date
@@ -344,38 +237,38 @@ server <- function(input, output,session) {
   })
   
   output$downloadData <- downloadHandler(
-  filename = function() {
-    tryCatch({
-    
-    #station=as.character(input$Station)
-    startdate<-as.character(min(as_date(D$documents[[1]]$TimeStamp)))
-    enddate<-as.character(max(as_date(D$documents[[1]]$TimeStamp)))
-    gather<-input$gather
-    round=input$round
-    nstat=D$documents[[1]]$SCODE %>% unique %>% length %>% as.character 
-    #paste0(station,'_',round,'_',startdate,'_',enddate,'.csv')
-    paste0(nstat,'stat','_',startdate,'_',enddate,'_',round,'_',gather,'.csv')#,round
+    filename = function() {
+      tryCatch({
+        
+        #station=as.character(input$Station)
+        startdate<-as.character(min(as_date(D$documents[[1]]$TimeStamp)))
+        enddate<-as.character(max(as_date(D$documents[[1]]$TimeStamp)))
+        gather<-input$gather
+        round=input$round
+        nstat=D$documents[[1]]$SCODE %>% unique %>% length %>% as.character 
+        #paste0(station,'_',round,'_',startdate,'_',enddate,'.csv')
+        paste0(nstat,'stat','_',startdate,'_',enddate,'_',round,'_',gather,'.csv')#,round
+        
+      }, error = function(e){"error.csv"})#
       
-    }, error = function(e){"error.csv"})#
-  
-  },
-  content = function(con) {
-    
-    round<-input$round
-    gather<-input$gather
-    df=D$documents[[1]] 
-    
-    if(gather=="wide"){
-      spread=TRUE}else{
-      spread=FALSE
+    },
+    content = function(con) {
+      
+      round<-input$round
+      gather<-input$gather
+      df=D$documents[[1]] 
+      
+      if(gather=="wide"){
+        spread=TRUE}else{
+          spread=FALSE
+        }
+      
+      db=resample_provBz_data(df=df,round=round,spread=spread)
+      write.csv(x=db,file =  con,quote = F,row.names = F,na = "NA")
+      
     }
     
-    db=resample_provBz_data(df=df,round=round,spread=spread)
-    write.csv(x=db,file =  con,quote = F,row.names = F,na = "NA")
-    
-  }
-  
-)
+  )
   
   outputOptions(output, 'tablebuilt', suspendWhenHidden=FALSE)
   outputOptions(output, 'rightdate', suspendWhenHidden=FALSE)
