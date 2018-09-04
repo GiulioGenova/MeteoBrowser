41293fbb42f043c9b6f5c54b6606b7b3bfd2b86f
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-04 15:56:42 +0200

    Update download_resample.R

--- app/download_resample.R
+++ app/download_resample.R
@@ -87,8 +87,24 @@ resample_provBz_data<-function(df,round="hour",spread=FALSE){
         gather(Variable, Value, -Sensor,-TimeStamp,-SCODE,-NAME_D,-NAME_I,-NAME_L,-NAME_E,-ALT,-LONG,-LAT) %>%
         unite(Sensor, Sensor, Variable,sep="") %>% 
         ungroup
+    
+      db_wind<-df%>%filter(Sensor%in%c("WG")) %>% 
+        group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor,NAME_D,NAME_I,NAME_L,NAME_E,ALT,LONG,LAT)%>%
+        dplyr::mutate(Value=ifelse(Value>0 & Value<=22.5,"N",
+                           ifelse(Value>22.5 & Value<=67.5,"NE",
+                                  ifelse(Value>67.5 & Value<=112.5,"E",
+                                         ifelse(Value>112.5 & Value<=157.5,"SE",
+                                                ifelse(Value>157.5 & Value<=202.5,"S",
+                                                       ifelse(Value>202.5 & Value<=247.5,"SW",
+                                                              ifelse(Value>247.5 & Value<=292.5,"W",
+                                                                     ifelse(Value>292.5 & Value<=337.5,"NW",
+                                                                            ifelse(Value>292.5 & Value<=360,"N",NA))))))))))%>%
+        summarise(prevalentDir=as.character(sort(Value, decreasing=T)[1])) %>% 
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE,-NAME_D,-NAME_I,-NAME_L,-NAME_E,-ALT,-LONG,-LAT) %>%
+        unite(Sensor, Sensor, Variable,sep="") %>% 
+        ungroup
       
-      db_final<-bind_rows(db_sum,db_mean,db_min_max) %>% mutate(Value=ifelse(Value%in%c(-Inf,Inf,NaN),NA,Value))
+      db_final<-bind_rows(db_sum,db_mean,db_min_max,db_wind) %>% mutate(Value=ifelse(Value%in%c(-Inf,Inf,NaN),NA,Value))
 
     
     #db_final<-df%>%
