d90b1d296b585f0e991b9c4353c57a21cc0b01ad
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-05 19:50:26 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -109,9 +109,9 @@ server <- function(input, output,session) {
   
   output$table<-DT::renderDT({
     if(input$language=="it"){
-    tot_tab<-tot_tab[,-"NAME_D"]
+    tot_tab<-tot_tab%>%dplyr::select(-NAME_D)
     }else{
-    tot_tab<-tot_tab[,-"NAME_I"]
+    tot_tab<-tot_tab%>%dplyr::select(-NAME_I)
     }
     datatable(tot_tab, filter = 'top',rownames=F,selection="none",
               options = list(autoWidth = F,scrollX=T)
