2037acd55efd94b0e4a867148567822a2b4758cd
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-05 19:35:28 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -151,7 +151,12 @@ server <- function(input, output,session) {
     
     station<-unique(tot_tab$SCODE[ids])%>%as.character
     sensors<-unique(tot_tab$TYPE[ids])%>%as.character
-    stationName<-unique(tot_tab$NAME_D[ids])%>%as.character
+    if(input$lenguage=="it"){
+      stationName<-unique(tot_tab$NAME_I[ids])%>%as.character
+      }else {
+       stationName<-unique(tot_tab$NAME_D[ids])%>%as.character
+    
+    }
     #########################################################
     if(input$spatialSelection){#FALSE
       #req(input$map_draw_stop)
@@ -180,7 +185,15 @@ server <- function(input, output,session) {
       sp_sel<-stations_sp %>% dplyr::filter(row_number()%in%which(!is.na(selected_stats)))
       
       station<-unique(sp_sel$SCODE) %>% as.character
-      stationName<-unique(sp_sel$NAME_D) %>% as.character
+      
+        if(input$lenguage=="it"){
+      stationName<-unique(sp_sel$NAME_I) %>% as.character
+      }else {
+       stationName<-unique(sp_sel$NAME_D) %>% as.character
+    
+    }
+        
+        stationName<-unique(sp_sel$NAME_D) %>% as.character
       
       filterForSensor<-tot_tab[ids,]%>% dplyr::filter(SCODE%in%station)
       sensors<-unique(filterForSensor$TYPE)%>%as.character
