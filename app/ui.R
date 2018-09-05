9649e3704866de5b8bc68b8bf3e8819249d1b0c6
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-05 17:40:40 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -112,7 +112,7 @@ ui <- dashboardPage(#useShinyjs(),
 
                 #helpText("Select stations and parameters you want to dowload by filtering the table below.",
                 #         "To stop the download refresh the page"),
-                verbatimTextOutput("tableInstructions"),
+                helpText("tableInstructions"),
                 DTOutput('table')
                
               )
