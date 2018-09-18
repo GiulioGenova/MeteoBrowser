744dfcd462bf2caba285c956650bea688477c035
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-18 18:02:11 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -341,8 +341,8 @@ server <- function(input, output,session) {
     return()
   })
   
-  
-  output$map<-renderLeaflet({
+  output$map<-renderUI({
+  renderLeaflet({
     ids<-input$table_rows_all
     station<-unique(tot_tab$SCODE[ids])%>%as.character
     stations_sel<-getMeteoStat(format = "spatial")%>%filter(SCODE%in%station)#NAME_D%in%input$Station get spatial stations database (Province) with the seleced SCODEs
@@ -378,7 +378,7 @@ server <- function(input, output,session) {
     m
     
   })
-  
+  })
   drawnshapes <- list()
   
   # we are fortunate here since we get an event
