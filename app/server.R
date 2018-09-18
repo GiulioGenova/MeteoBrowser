8d70012a0bc3b842fc5dc63da4ef41a1909810fb
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-18 18:30:13 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -130,11 +130,11 @@ server <- function(input, output,session) {
                                               ))
    })
   
-  output$spatSel<-renderUI({
-    
-  checkboxInput("spatialSelection",label = h4(as.character(translation[grep("spatialSelection",translation$key),input$language])),
-                                 value = FALSE)
-  })
+  #output$spatSel<-renderUI({
+  #  
+  #checkboxInput("spatialSelection",label = h4(as.character(translation[grep("spatialSelection",translation$key),input$language])),
+  #                               value = FALSE)
+  #})
   
   
   output$message<-renderText({
