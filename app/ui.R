8416dad6408b9dc8ebbd5b5c11c750fb73e7330e
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-20 09:50:43 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -59,7 +59,7 @@ ui <- dashboardPage(#useShinyjs(),
       ),
     #title =loadingLogo('http://www.eurac.edu/Style%20Library/','logoEURAC.jpg','buffpowa.gif')
     title = tags$a(href='http://www.eurac.edu/',
-         tags$img(src='Style%20Library/logoEURAC.jpg'))#,
+         tags$img(src='http://www.eurac.edu/Style%20Library/logoEURAC.jpg'))#,
     
   #  titleWidth = 320
   ),
