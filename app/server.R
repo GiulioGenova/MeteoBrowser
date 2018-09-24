9384eef55fa3e2eccc76c95d5c4f96d598c28f5e
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-24 16:25:47 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -92,13 +92,13 @@ server <- function(input, output,session) {
   
   # UI
   
-  #output$Data  <- renderUI({
-  #menuItem("Data overwiev", tabName = "Data", icon = icon("bar-chart-o"))
-  #})
+  output$Data  <- renderUI({
+  menuItem("Data overwiev", tabName = "Data", icon = icon("bar-chart-o"))
+  })
   
-  #output$about  <- renderUI({
-  #menuItem("README", tabName = "about", icon = icon("info-circle"))
-  #})
+  output$about  <- renderUI({
+  menuItem("README", tabName = "about", icon = icon("info-circle"))
+  })
   
   output$refresh  <- renderUI({
     #datestart<- input$daterange[1] %>% as.character %>% as_date
