1979578078124b3ee3e4566e71e9db5edf7dacf0
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-25 00:48:50 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -46,7 +46,7 @@ scr <- tags$script(HTML(
 
 # then our new app can do something like this
 
-header <- dashboardHeader(titleWidth = 580)
+header <- dashboardHeader(titleWidth = 480)
 anchor <- tags$a(tags$style(HTML(".tit { color: black; padding-right: 5px;font-weight: bold;font-family: Arial;}")),#;font-size: 1vw;;font-size: 1.05em
                  tags$style(HTML(".tit:hover { color: #cd4e37;}")),
                  href='http://www.eurac.edu/',
