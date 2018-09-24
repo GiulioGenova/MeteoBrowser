083d5863ad8c0c5da37ed3f9955e14283ff2d1f8
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-24 18:09:37 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -71,7 +71,7 @@ ui <- dashboardPage(#useShinyjs(),
     )
     ,
     #title =loadingLogo('http://www.eurac.edu/Style%20Library/','logoEURAC.jpg','buffpowa.gif')
-    title = tags$a(href='http://www.eurac.edu/',
+    title = tags$a(href='http://www.eurac.edu/',title= "Meteo Browser",
                    #tags$img(src='http://www.eurac.edu/Style%20Library/logoEURAC.jpg'))#,
                    tags$img(src='logoMeteoBrowser.jpg'))
     #  titleWidth = 320
