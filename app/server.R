34397c256942e015ed6eaf53cd8b1cd10807db9a
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-24 09:54:00 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -401,7 +401,29 @@ server <- function(input, output,session) {
     stations_sel<-getMeteoStat(format = "spatial")%>%filter(SCODE%in%station)#NAME_D%in%input$Station get spatial stations database (Province) with the seleced SCODEs
     stations_sel<-left_join(stations_sel,se_spread)
     m<-plotMeteoLeaflet(stations_sel)
-    
+    m<- m %>% addAwesomeMarkers(lng = stations$LONG %>% as.character %>% as.numeric, lat = stations$LAT %>% 
+                        as.character %>% as.numeric, icon = c1, popup = paste("Code:",stations$SCODE, "<br>", 
+                                                                              "Name GER:", stations$NAME_D,"<br>",
+                                                                              "Name ITA:", stations$NAME_I, "<br>",
+                                                                              "Altitude:",stations$ALT, "<br>",
+                                                                              "<br>",
+                                                                              "Latest recorded measurements:", "<br>",
+                                                                              "Air temperature:",stations$LT, "<br>",
+                                                                              "Relative humidity:",stations$LF, "<br>",
+                                                                              
+                                                                              "Precipitation:",stations$N, "<br>",
+                                                                              "Wind speed:",stations$WG, "<br>",
+                                                                              "Wind direction:",stations$WR, "<br>",
+                                                                              "Wind guts:",stations$WG.BOE, "<br>",
+                                                                              "Atmospheric perssion:",stations$LD.RED, "<br>",
+                                                                              "Solar Radiation:",stations$GS, "<br>",
+                                                                              "Sunshine hours:",stations$SD, "<br>",
+                                                                              "Snow height:",stations$HS, "<br>",
+                                                                              "Water temperature:",stations$WT, "<br>",
+                                                                              "Water flow:",stations$Q, "<br>",
+                                                                              "Water level:",stations$W, "<br>"#,
+                                                                              #"Ground water level:",stations$WT, "W.ABST"
+))
 # "output.spatialSelection"
 # input$spatialSelection   
     if(input$spatialSelection){#FALSE
