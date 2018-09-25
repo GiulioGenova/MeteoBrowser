1b75a6d191f2ad5132039cfb7de7b116952082ee
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-25 12:21:34 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -104,8 +104,9 @@ server <- function(input, output,session) {
   # UI
   output$save  <- renderMenu({
   conditionalPanel(condition = "output.tablebuilt",br(),#"input.daterange[1]<=input.daterange[2]"
-                 tags$span(downloadButton('downloadData', h4('Save as csv'),class="btn-danger" ),
+                 tags$span(style="display: inline-block;horizontal-align:top;",downloadButton('downloadData', h4('Save as csv'),class="btn-danger" ),
                  radioButtons(inputId = "csvjson",label = "table type",choices = list("csv","json"))))
+
 })
     
   output$Data  <- renderMenu({
