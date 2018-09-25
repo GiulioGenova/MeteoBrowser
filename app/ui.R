724101d9c86596585c9476c6203ddf66ff7d44a9
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-25 11:24:42 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -47,11 +47,11 @@ scr <- tags$script(HTML(
 # then our new app can do something like this
 
 header <- dashboardHeader(titleWidth = 480)
-#anchor <- tags$a(tags$style(HTML(".tit { color: black; padding-right: 5px;font-weight: bold;font-family: Arial;}")),#;font-size: 1vw;;font-size: 1.05em
-#                 tags$style(HTML(".tit:hover { color: #cd4e37;}")),
-#                 href='http://www.eurac.edu/',
-#                 tags$img(src='http://www.eurac.edu/Style%20Library/logoEURAC.jpg'),#, height='60', width='50'
-#                 'Meteo Browser South Tyrol',class='tit')
+anchor <- tags$a(tags$style(HTML(".tit { color: black; padding-right: 5px;font-weight: bold;font-family: Arial;}")),#;font-size: 1vw;;font-size: 1.05em
+                 tags$style(HTML(".tit:hover { color: #cd4e37;}")),
+                 href='http://www.eurac.edu/',
+                 tags$img(src='http://www.eurac.edu/Style%20Library/logoEURAC.jpg'),#, height='60', width='50'
+                 'Meteo Browser South Tyrol',class='tit')
 
 stile<-tags$li(class = "dropdown",
            tags$style( ".skin-blue .main-header .logo {
@@ -81,7 +81,7 @@ tags$a(tags$style(HTML(".tit { color: black; padding-right: 5px;font-weight: bol
 
 header$children[[2]]$children <- tags$div(
     tags$head(tags$style(HTML(".name { color: black }"))),
-    stile,#anchor,
+    stile,anchor,
     class = 'name')
 
 
