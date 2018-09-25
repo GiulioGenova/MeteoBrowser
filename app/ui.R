1ba42136ba5d7c1d952260af51d038f819a952cf
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-25 11:31:29 +0200

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -70,12 +70,7 @@ stile<-tags$li(class = "dropdown",
             tags$style(".main-header .logo {height: 75px ; padding-top: 10px}"),
             tags$style(".skin-blue .main-header .navbar .sidebar-toggle:hover{
                         background-color:  	#cd4e37;
-                                            }"),
-tags$a(tags$style(HTML(".tit { color: black; padding-right: 5px;font-weight: bold;font-family: Arial;}")),#;font-size: 1vw;;font-size: 1.05em
-                 tags$style(HTML(".tit:hover { color: #cd4e37;}")),
-                 href='http://www.eurac.edu/',
-                 tags$img(src='http://www.eurac.edu/Style%20Library/logoEURAC.jpg'),#, height='60', width='50'
-                 'Meteo Browser South Tyrol',class='tit')
+                                            }")
 
     )
 
