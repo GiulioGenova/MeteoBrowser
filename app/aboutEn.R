7f4b8b8db61100fed0bd8f30101b5b5c5144f510
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-26 16:06:43 +0200

    Update aboutEn.R

--- app/aboutEn.R
+++ app/aboutEn.R
@@ -9,18 +9,18 @@ tags$html(
     p(a( href='http://daten.buergernetz.bz.it/it/dataset/misure-meteo-e-idrografiche',
          target='_blank','Open Data Suedtirol - Meteo data web service' )),
     p("Once selected the stations and sensors by filtering the table and/or drawing a polygon on the map and chosen 
-a date range you are ready to download (the dataset starts in the beginning of 2016). 
-When download is complete, a \x22save\x22 icon pops up and you can choose to save a csv or a json file. 
+a date range you are ready to download (the dataset starts in the beginning of 2016)."), 
+p("When download is complete, a \x22save\x22 icon pops up and you can choose to save a csv or a json file. 
 You can choose between different time aggregations. \x22raw\x22 preserves the timestamp of the sensor you are 
 downloading (usually 5 or 10 minutes). For the other time aggregation options you will get different statistics
-for each measurement in the selected timespan. 
-All measurements willa have a count (the sum) of No Data in the timespan selected.
-All measurements besides precipitation and wind direction will also have a mean value. 
-Precipitation and Air temperature will also have the sum value. 
-Air temperature and relative humidity will also have min and max values.
-Wind direction will be reclassified with the following values and the most frequent value will be displayed: 
-1 = N; 2 = NE; 3 = E; 4 = SE; 5 = S; 6 = SW; 7 = W; 8 = NW. 
-You can also choose between \x22wide\x22 or \x22long\x22 data.
+for each measurement in the selected timespan."), 
+p("All measurements willa have a count (the sum) of No Data in the timespan selected."),
+p("All measurements besides precipitation and wind direction will also have a mean value. "),
+p("Precipitation and Air temperature will also have the sum value."), 
+p("Air temperature and relative humidity will also have min and max values."),
+p("Wind direction will be reclassified with the following values and the most frequent value will be displayed:"), 
+p("1 = N; 2 = NE; 3 = E; 4 = SE; 5 = S; 6 = SW; 7 = W; 8 = NW."), 
+p("You can also choose between \x22wide\x22 or \x22long\x22 data.
 In wide data you get a column for every sensor, in long data you get a factor column called \x22Sensor\x22 and a column
 \x22Value\x22 which stores the measurements."),
     br(),
