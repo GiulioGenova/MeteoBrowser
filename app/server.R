f0278696a04346871ed9c2e2d09194061e1ab236
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-27 10:01:33 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -60,7 +60,7 @@ library(sp)
 
 source(file.path(getwd(),"download_resample.R"))
 source(file.path(getwd(),"MonalisR.R"))
-translation<-read.csv(file.path(getwd(),"translation.csv"),header = T,sep = ",",stringsAsFactors = F)
+translation<-read.csv(file.path(getwd(),"translationNew.csv"),header = T,sep = ",",stringsAsFactors = F)
 
 leaf.proj <- "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 LL <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
