98901583c08e879041009065ef1a62d7305f8635
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-27 12:29:27 +0200

    Update server.R

--- app/server.R
+++ app/server.R
@@ -60,7 +60,8 @@ library(sp)
 
 source(file.path(getwd(),"download_resample.R"))
 source(file.path(getwd(),"MonalisR.R"))
-translation<-read.csv(file.path(getwd(),"translationNew.csv"),header = T,sep = ",",stringsAsFactors = F,encoding = "UTF-8")#
+load(file.path(getwd(),"translationNew.R"))
+#translation<-read.csv(file.path(getwd(),"translationNew.csv"),header = T,sep = ",",stringsAsFactors = F,encoding = "UTF-8")#
 
 tr <- function(key,l,t=translation){ # translates text into current language
   x<-as.character(t[grep(key,t$key),l])
