285ae4ed4463121115775000375fbd0792d13391
Giulio Genova <39799547+GiulioGenova@users.noreply.github.com>
2018-09-27 15:07:15 +0200

    Add files via upload

--- app/about_IT.R
+++ app/about_IT.R
@@ -10,16 +10,16 @@ tags$html(
     p(a( href='http://daten.buergernetz.bz.it/it/dataset/misure-meteo-e-idrografiche',
          target='_blank','Open Data Alto Adige - Meteo data web service' )),
     p("Una volta selezionate le stazioni e i sensori, filtrando la tabella e / o disegnando un poligono sulla mappa, e scelto l'arco temporale d'interesse (i dati disponibili si riferiscono agli ultimi tre anni)."), 
-    p("Quando lo scarico dati \xe8 completato, un'icona \x22salva\x22 vine mostrata e si può scegliere di salvare la tabella di dati in formato csv o json. 
+    p("Quando lo scarico dati \xe8 completato, un'icona \x22salva\x22 vine mostrata e si pu\xf2 scegliere di salvare la tabella di dati in formato csv o json. 
       Si puo scegliere tra diverse aggregazioni temporali. \x22grezzo\x22 mantiene l'originale risoluzione d'acquisizione dei dati
       (tipicamente 5 o 10 minuti). Per le altre opzioni di aggregazione temporale (medie orarie, giornaliere, ecc.) si avranno diverse statistiche per le diverse variabili."), 
     p("Per tutte le variabili misurate si avr\xe0 il numero totale di dati mancanti per il periodo selezionato."),
     p("per tutte le variabili misurate, fatta eccezione per la precipitazione e direzione del vento \xe8 compiuta la media aritmetica per la specifica aggregazione temporale. "),
     p("Precipitazione e temperatura dell'aria avranno anche la somma totale riferita alla specifica aggregazione temporale."), 
     p("Temperatura dell'aria e umidit\xe0 relativa avranno anche i valori minimi e massimi riferiti alla specifica aggregazione temporale."),
-    p("La direzione del vento \xe8 riclassificata con i seguenti codici numerici e il píu frequente sar\xe0 assegnato:"), 
+    p("La direzione del vento \xe8 riclassificata con i seguenti codici numerici e il pi\xf9 frequente sar\xe0 assegnato:"), 
     p("1 = N; 2 = NE; 3 = E; 4 = SE; 5 = S; 6 = SO; 7 = O; 8 = NO."), 
-    p("Si può scegliere tra diversi formati di tabella,\x22larga\x22 or \x22lunga\x22.
+    p("Si pu\xf2 scegliere tra diversi formati di tabella,\x22larga\x22 or \x22lunga\x22.
       Nel formato di tabella largo si ottengono una colonna per ogni variabile misurata. Nel formato di tabella lungo si ottengono  una colonna che identifica la misura \x22Sensor\x22 e una colonna 
       \x22Value\x22 che corrisponde alle misure."),
     br(),
@@ -50,7 +50,7 @@ tags$html(
     p("2018-09-26"),
     br(),
     h3("Ringraziamenti"),
-    p("Rignaziamo Mattia Rossi il quale ha sviluppato il pacchetto R\x22MonalisR\x22 che svolge un ruolo fondamentale nel backend di questa pplicazione. Il pacchetto puó essere trovato al seguente link:"),
+    p("Rignaziamo Mattia Rossi il quale ha sviluppato il pacchetto R\x22MonalisR\x22 che svolge un ruolo fondamentale nel backend di questa pplicazione. Il pacchetto pu\xf9 essere trovato al seguente link:"),
     
     p(a( href='https://gitlab.inf.unibz.it/earth_observation_public/MonalisR', 
          target='_blank','MonalisR'))
