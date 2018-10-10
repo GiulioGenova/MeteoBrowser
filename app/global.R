52149531960f10496496dd322d92d07f06c1b6f0
GiulioGenova <giuliogenova89@gmail.com>
2018-10-10 18:10:01 +0200

    changed logo and some styling

--- app/global.R
+++ app/global.R
@@ -43,13 +43,14 @@ library(DT)
 source(file.path(getwd(),"download_resample.R"))
 source(file.path(getwd(),"MonalisR.R"))
 
-header <- dashboardHeader(titleWidth = 480)
+header <- dashboardHeader(titleWidth = 350)
 
 anchor <- tags$a(href='http://www.eurac.edu/',
-                 tags$img(style="vertical-align: bottom;width: 270px;padding-right: 10px;",
+                 tags$img(style="vertical-align: bottom;width: 320px;",#padding-right: 10px;
                           #src='http://www.eurac.edu/Style%20Library/logoEURAC.jpg'),#, height='60', width='50'
-                          src='LogoProvEurac.PNG'),
-                 'Meteo Browser South Tyrol',class='tit')
+                          src='LogoProvEuracN.PNG'),
+                 #'Meteo Browser South Tyrol',
+                 class='tit')
 
 header$children[[2]]$children <- tags$div(anchor,class = 'name')
 
