f6da8e52cf19513777d6de2e088a0d817acf4098
GiulioGenova <giuliogenova89@gmail.com>
2019-02-27 11:42:02 +0100

    avoiding to download the station table at every startup - only the sensors with latest measurements are taken

--- R/initializeApp.R
+++ R/initializeApp.R
@@ -24,42 +24,41 @@ initializeApp <- function(){
 
   # Building the table of stations
 
-  url <- "http://daten.buergernetz.bz.it/services/meteo/v1/sensors"
   u <- tryCatch({
-    GET(url) %>% content
+    GET("http://daten.buergernetz.bz.it/services/meteo/v1/sensors") %>% content
   }, error = function(e){NULL})#
 
   if(!is.null(u)){
     se<-bind_rows(u)
     se <-se[!duplicated(se[ , 1:2 ]), ]
-    st<-getMeteoStat()
-    legend_tab<-full_join(st,se)%>%#dplyr::select(-NAME_L,-NAME_E,-DESC_L,-DATE,VALUE,-LAT,-LONG,-VALUE)%>%
-      mutate_if(is.character, funs(as.factor(.)))%>%
-      mutate(
-        id=row_number(),
-        DESC_E=as.factor(ifelse(DESC_D=="relative Luftfeuchte","Relative humidity",
-                                ifelse(DESC_D=="Niederschlag","Precipitation",
-                                       ifelse(DESC_D=="Windgeschwindigkeit","Wind speed",
-                                              ifelse(DESC_D=="Windrichtung","Wind direction",
-                                                     ifelse(DESC_D=="Windgeschwindigkeit Böe","Wind gust",
-                                                            ifelse(DESC_D=="Luftdruck","Atmospheric pression",
-                                                                   ifelse(DESC_D=="Globalstrahlung","Solar Radiation",
-                                                                          ifelse(DESC_D=="Sonnenscheindauer","Sunshine hours",
-                                                                                 ifelse(DESC_D=="Schneehöhe","Snow height",
-                                                                                        ifelse(DESC_D=="Wassertemperatur","Water temperature",
-                                                                                               ifelse(DESC_D=="Lufttemperatur","Air temperature",
-                                                                                                      ifelse(DESC_D=="Durchfluss","Water flow",
-                                                                                                             ifelse(DESC_D=="Wasserstand","Water level",
-                                                                                                                    ifelse(DESC_D=="Grundwasserstand","Groundwater level","unknown"))))))))))))))))
-
-
-    tot_tab<<-legend_tab%>%select(-NAME_L,-DESC_L,-DATE,-LAT,-LONG,-VALUE) %>%
-      mutate(NAME = paste(NAME_D,NAME_I,sep=" / ")) %>%
-      mutate(DESC_D = paste(DESC_D,UNIT,sep=" - ")) %>%
-      mutate(DESC_I = paste(DESC_I,UNIT,sep=" - ")) %>%
-      unite(DESC_E,DESC_E,UNIT,sep=" - ")
-
-    legend_tab<<-legend_tab %>% select(-DATE,-VALUE)
+    # st<-getMeteoStat()
+    # legend_tab<-full_join(st,se)%>%#dplyr::select(-NAME_L,-NAME_E,-DESC_L,-DATE,VALUE,-LAT,-LONG,-VALUE)%>%
+    #   mutate_if(is.character, funs(as.factor(.)))%>%
+    #   mutate(
+    #     id=row_number(),
+    #     DESC_E=as.factor(ifelse(DESC_D=="relative Luftfeuchte","Relative humidity",
+    #                             ifelse(DESC_D=="Niederschlag","Precipitation",
+    #                                    ifelse(DESC_D=="Windgeschwindigkeit","Wind speed",
+    #                                           ifelse(DESC_D=="Windrichtung","Wind direction",
+    #                                                  ifelse(DESC_D=="Windgeschwindigkeit Böe","Wind gust",
+    #                                                         ifelse(DESC_D=="Luftdruck","Atmospheric pression",
+    #                                                                ifelse(DESC_D=="Globalstrahlung","Solar Radiation",
+    #                                                                       ifelse(DESC_D=="Sonnenscheindauer","Sunshine hours",
+    #                                                                              ifelse(DESC_D=="Schneehöhe","Snow height",
+    #                                                                                     ifelse(DESC_D=="Wassertemperatur","Water temperature",
+    #                                                                                            ifelse(DESC_D=="Lufttemperatur","Air temperature",
+    #                                                                                                   ifelse(DESC_D=="Durchfluss","Water flow",
+    #                                                                                                          ifelse(DESC_D=="Wasserstand","Water level",
+    #                                                                                                                 ifelse(DESC_D=="Grundwasserstand","Groundwater level","unknown"))))))))))))))))
+    #
+    #
+    # tot_tab<<-legend_tab%>%select(-NAME_L,-DESC_L,-DATE,-LAT,-LONG,-VALUE) %>%
+    #   mutate(NAME = paste(NAME_D,NAME_I,sep=" / ")) %>%
+    #   mutate(DESC_D = paste(DESC_D,UNIT,sep=" - ")) %>%
+    #   mutate(DESC_I = paste(DESC_I,UNIT,sep=" - ")) %>%
+    #   unite(DESC_E,DESC_E,UNIT,sep=" - ")
+    #
+    # legend_tab<<-legend_tab %>% select(-DATE,-VALUE)
 
     se_spread <- se  %>%
       select(SCODE,TYPE,UNIT,VALUE,DATE) %>%
@@ -69,12 +68,17 @@ initializeApp <- function(){
       unite(VALUE,VALUE,DATE,sep=" ") %>%
       spread(TYPE,VALUE)
 
-    sspat<<-getMeteoStat(format = "spatial")
+    #sspat<<-getMeteoStat(format = "spatial")
+    tot_tab<<- tot_tab_def
+    sspat<<- sspat_def
+
     se_spread<<-left_join(sspat,se_spread)
+
   } else {
     tot_tab<<- tot_tab_def
-    legend_tab<<- legend_tab_def
+    #legend_tab<<- legend_tab_def
     sspat<<- sspat_def
+
     se_spread<<- se_spread_def
 
     waring_website<-"Province Server not available"
@@ -97,6 +101,4 @@ initializeApp <- function(){
 
   refcols <<- c("TimeStamp", "NAME")#,"SCODE"
 
-  leaf.proj <<- "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-  LL <<- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 }
--- inst/shinyApps/MeteoBrowser/server.R
+++ inst/shinyApps/MeteoBrowser/server.R
@@ -509,10 +509,10 @@ server <- function(input, output,session) {
           sp <- SpatialPolygons(list(Polygons(list(drawn_polygon),"drawn_polygon")))
 
           # set coords as latlong then transform to leaflet projection
-          proj4string(sp) <- LL
-          polyre <- spTransform(sp, leaf.proj)
+          proj4string(sp) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+          polyre <- spTransform(sp, "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
 
-          stations_sp<-spTransform(stations_sp,leaf.proj)
+          stations_sp<-spTransform(stations_sp,"+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
 
           selected_stats <- stations_sp %over% polyre
 
