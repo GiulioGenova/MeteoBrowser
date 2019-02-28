dc6bece99e6709f6d49ff0f033f0a7d30c68a2db
GiulioGenova <giuliogenova89@gmail.com>
2019-02-28 14:28:07 +0100

    apply ceiling_date to hour resample and floor_date to the others

--- R/resample_provBz_data.R
+++ R/resample_provBz_data.R
@@ -38,48 +38,98 @@ resample_provBz_data<-function(df,round="hour"){
 
     windList = c("WR")
 
-    db_sum<-df%>%filter(Sensor%in%sumList) %>% #"LT",
-      group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
-      summarise(sum=round(sum(Value,na.rm = T),2)) %>%
-      gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
-      unite(Sensor, Sensor, Variable,sep="_") %>%
-      ungroup
+    if(round=="hour"){
+
+      db_sum<-df%>%filter(Sensor%in%sumList) %>% #"LT",
+        group_by(TimeStamp=ceiling_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        summarise(sum=round(sum(Value,na.rm = T),2)) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
 
 
-    db_mean<-df%>%filter(!Sensor%in%notmeanList) %>%
-      group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
-      summarise(mean=round(mean(Value,na.rm = T),2)) %>%
-      gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
-      unite(Sensor, Sensor, Variable,sep="_") %>%
-      ungroup
+      db_mean<-df%>%filter(!Sensor%in%notmeanList) %>%
+        group_by(TimeStamp=ceiling_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        summarise(mean=round(mean(Value,na.rm = T),2)) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
 
-    db_min_max<-df%>%filter(Sensor%in%minmaxList) %>%
-      group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
-      summarise(min=round(min(Value,na.rm = T),2),
-                max=round(max(Value,na.rm = T),2)) %>%
-      gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
-      unite(Sensor, Sensor, Variable,sep="_") %>%
-      ungroup
+      db_min_max<-df%>%filter(Sensor%in%minmaxList) %>%
+        group_by(TimeStamp=ceiling_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        summarise(min=round(min(Value,na.rm = T),2),
+                  max=round(max(Value,na.rm = T),2)) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
 
 
-    db_wind<-df%>%filter(Sensor%in%windList) %>%
-      mutate(Value=ifelse(Value>0 & Value<=22.5,1,
-                          ifelse(Value>22.5 & Value<=67.5,2,
-                                 ifelse(Value>67.5 & Value<=112.5,3,
-                                        ifelse(Value>112.5 & Value<=157.5,4,
-                                               ifelse(Value>157.5 & Value<=202.5,5,
-                                                      ifelse(Value>202.5 & Value<=247.5,6,
-                                                             ifelse(Value>247.5 & Value<=292.5,7,
-                                                                    ifelse(Value>292.5 & Value<=337.5,8,
-                                                                           ifelse(Value>292.5 & Value<=360,1,NA))))))))))%>%
+      db_wind<-df%>%filter(Sensor%in%windList) %>%
+        mutate(Value=ifelse(Value>0 & Value<=22.5,1,
+                            ifelse(Value>22.5 & Value<=67.5,2,
+                                   ifelse(Value>67.5 & Value<=112.5,3,
+                                          ifelse(Value>112.5 & Value<=157.5,4,
+                                                 ifelse(Value>157.5 & Value<=202.5,5,
+                                                        ifelse(Value>202.5 & Value<=247.5,6,
+                                                               ifelse(Value>247.5 & Value<=292.5,7,
+                                                                      ifelse(Value>292.5 & Value<=337.5,8,
+                                                                             ifelse(Value>292.5 & Value<=360,1,NA))))))))))%>%
 
-      group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        group_by(TimeStamp=ceiling_date(TimeStamp,unit = round),SCODE,Sensor)%>%
 
-      summarise(Dir=as.numeric(names(which.max(table(Value,useNA = "no"))))) %>%
-      gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
-      unite(Sensor, Sensor, Variable,sep="_") %>%
-      ungroup
+        summarise(Dir=as.numeric(names(which.max(table(Value,useNA = "no"))))) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
 
+
+    }else{
+
+
+      db_sum<-df%>%filter(Sensor%in%sumList) %>% #"LT",
+        group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        summarise(sum=round(sum(Value,na.rm = T),2)) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
+
+
+      db_mean<-df%>%filter(!Sensor%in%notmeanList) %>%
+        group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        summarise(mean=round(mean(Value,na.rm = T),2)) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
+
+      db_min_max<-df%>%filter(Sensor%in%minmaxList) %>%
+        group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+        summarise(min=round(min(Value,na.rm = T),2),
+                  max=round(max(Value,na.rm = T),2)) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
+
+
+      db_wind<-df%>%filter(Sensor%in%windList) %>%
+        mutate(Value=ifelse(Value>0 & Value<=22.5,1,
+                            ifelse(Value>22.5 & Value<=67.5,2,
+                                   ifelse(Value>67.5 & Value<=112.5,3,
+                                          ifelse(Value>112.5 & Value<=157.5,4,
+                                                 ifelse(Value>157.5 & Value<=202.5,5,
+                                                        ifelse(Value>202.5 & Value<=247.5,6,
+                                                               ifelse(Value>247.5 & Value<=292.5,7,
+                                                                      ifelse(Value>292.5 & Value<=337.5,8,
+                                                                             ifelse(Value>292.5 & Value<=360,1,NA))))))))))%>%
+
+        group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
+
+        summarise(Dir=as.numeric(names(which.max(table(Value,useNA = "no"))))) %>%
+        gather(Variable, Value, -Sensor,-TimeStamp,-SCODE) %>%
+        unite(Sensor, Sensor, Variable,sep="_") %>%
+        ungroup
+
+
+    }
 
     #           db_na<-df%>%
     #             group_by(TimeStamp=floor_date(TimeStamp,unit = round),SCODE,Sensor)%>%
