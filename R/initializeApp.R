c16321c321f440bb15eccbd4d44c63733076480e
GiulioGenova <giuliogenova89@gmail.com>
2019-08-10 15:01:37 +0200

    added back the download of the station and sensor tables from province API at startup
    (cherry picked from commit df1b584521a2a3b1bcb46d6ac32c509bb0813cd4)

--- R/initializeApp.R
+++ R/initializeApp.R
@@ -31,48 +31,49 @@ initializeApp <- function(){
   if(!is.null(u)){
     se<-bind_rows(u)
     se <-se[!duplicated(se[ , 1:2 ]), ]
-    # st<-getMeteoStat()
-    # legend_tab<-full_join(st,se)%>%#dplyr::select(-NAME_L,-NAME_E,-DESC_L,-DATE,VALUE,-LAT,-LONG,-VALUE)%>%
-    #   mutate_if(is.character, funs(as.factor(.)))%>%
-    #   mutate(
-    #     id=row_number(),
-    #     DESC_E=as.factor(ifelse(DESC_D=="relative Luftfeuchte","Relative humidity",
-    #                             ifelse(DESC_D=="Niederschlag","Precipitation",
-    #                                    ifelse(DESC_D=="Windgeschwindigkeit","Wind speed",
-    #                                           ifelse(DESC_D=="Windrichtung","Wind direction",
-    #                                                  ifelse(DESC_D=="Windgeschwindigkeit Böe","Wind gust",
-    #                                                         ifelse(DESC_D=="Luftdruck","Atmospheric pression",
-    #                                                                ifelse(DESC_D=="Globalstrahlung","Solar Radiation",
-    #                                                                       ifelse(DESC_D=="Sonnenscheindauer","Sunshine hours",
-    #                                                                              ifelse(DESC_D=="Schneehöhe","Snow height",
-    #                                                                                     ifelse(DESC_D=="Wassertemperatur","Water temperature",
-    #                                                                                            ifelse(DESC_D=="Lufttemperatur","Air temperature",
-    #                                                                                                   ifelse(DESC_D=="Durchfluss","Water flow",
-    #                                                                                                          ifelse(DESC_D=="Wasserstand","Water level",
-    #                                                                                                                 ifelse(DESC_D=="Grundwasserstand","Groundwater level","unknown"))))))))))))))))
-    #
-    #
-    # tot_tab<<-legend_tab%>%select(-NAME_L,-DESC_L,-DATE,-LAT,-LONG,-VALUE) %>%
-    #   mutate(NAME = paste(NAME_D,NAME_I,sep=" / ")) %>%
-    #   mutate(DESC_D = paste(DESC_D,UNIT,sep=" - ")) %>%
-    #   mutate(DESC_I = paste(DESC_I,UNIT,sep=" - ")) %>%
-    #   unite(DESC_E,DESC_E,UNIT,sep=" - ")
-    #
-    # legend_tab<<-legend_tab %>% select(-DATE,-VALUE)
+    st<-getMeteoStat()
+    legend_tab<-full_join(st,se)%>%#dplyr::select(-NAME_L,-NAME_E,-DESC_L,-DATE,VALUE,-LAT,-LONG,-VALUE)%>%
+      mutate_if(is.character, funs(as.factor(.)))%>%
+      mutate(
+        id=row_number(),
+        DESC_E=as.factor(case_when(DESC_D=="relative Luftfeuchte"~"Relative humidity",
+                                   DESC_D=="Niederschlag"~"Precipitation",
+                                   DESC_D=="Windgeschwindigkeit"~"Wind speed",
+                                   DESC_D=="Windrichtung"~"Wind direction",
+                                   DESC_D=="Windgeschwindigkeit Böe"~"Wind gust",
+                                   DESC_D=="Luftdruck"~"Atmospheric pression",
+                                   DESC_D=="Globalstrahlung"~"Solar Radiation",
+                                   DESC_D=="Sonnenscheindauer"~"Sunshine hours",
+                                   DESC_D=="Schneehöhe"~"Snow height",
+                                   DESC_D=="Wassertemperatur"~"Water temperature",
+                                   DESC_D=="Lufttemperatur"~"Air temperature",
+                                   DESC_D=="Durchfluss"~"Water flow",
+                                   DESC_D=="Wasserstand"~"Water level",
+                                   DESC_D=="Grundwasserstand"~"Groundwater level",
+                                   is.na(DESC_D) ~ "unknown")))
 
-    se_spread <- se  %>%
-      select(SCODE,TYPE,UNIT,VALUE,DATE) %>%
-      mutate(DATE=paste0("(",as_datetime(DATE),")"))%>%
-      unite(VALUE,VALUE,UNIT,sep=" ") %>%
-      mutate(VALUE=paste0("<b>",VALUE,"</b>"))%>%
-      unite(VALUE,VALUE,DATE,sep=" ") %>%
-      spread(TYPE,VALUE)
 
-    #sspat<<-getMeteoStat(format = "spatial")
-    tot_tab<<- tot_tab_def
-    sspat<<- sspat_def
+        tot_tab<<-legend_tab%>%select(-NAME_L,-DESC_L,-DATE,-LAT,-LONG,-VALUE) %>%
+          mutate(NAME = paste(NAME_D,NAME_I,sep=" / ")) %>%
+          mutate(DESC_D = paste(DESC_D,UNIT,sep=" - ")) %>%
+          mutate(DESC_I = paste(DESC_I,UNIT,sep=" - ")) %>%
+          unite(DESC_E,DESC_E,UNIT,sep=" - ")
 
-    se_spread<<-left_join(sspat,se_spread)
+        # legend_tab<<-legend_tab %>% select(-DATE,-VALUE)
+
+        se_spread <- se  %>%
+          select(SCODE,TYPE,UNIT,VALUE,DATE) %>%
+          mutate(DATE=paste0("(",as_datetime(DATE),")"))%>%
+          unite(VALUE,VALUE,UNIT,sep=" ") %>%
+          mutate(VALUE=paste0("<b>",VALUE,"</b>"))%>%
+          unite(VALUE,VALUE,DATE,sep=" ") %>%
+          spread(TYPE,VALUE)
+
+        sspat<<-getMeteoStat(format = "spatial")
+        #tot_tab<<- tot_tab_def
+        #sspat<<- sspat_def
+
+        se_spread<<-left_join(sspat,se_spread)
 
   } else {
     tot_tab<<- tot_tab_def
